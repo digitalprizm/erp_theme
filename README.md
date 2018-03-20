f9a1123dd2308ea7e741129c14a9788a3c7c0837
nishigandha30 <nish8800@gmail.com>
2018-03-20 12:47:09 +0530

    first commit of erp theme app

---
+++ README.md
@@ -0,0 +1,7 @@
+## erp_theme
+
+theme
+
+#### License
+
+MIT
---
+++ erp_theme/hooks.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from . import __version__ as app_version
+from frappe import _
+
+app_name = "erp_theme"
+app_title = "erp_theme"
+app_publisher = "dpi"
+app_description = "theme"
+app_icon = "icon-th-list"
+app_color = "#2E70D2"
+app_email = "nishigandha@digitalprizmglobal.com"
+app_license = "MIT"
+fixtures = ["Custom Field",
+"Property Setter",
+"Custom Script",
+"Print Format"]
+
+# Includes in <head>
+# ------------------
+# include js, css files in header of desk.html
+# app_include_css = "/assets/erp_theme/css/erp_theme.css"
+# app_include_js = "/assets/erp_theme/js/erp_theme.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/erp_theme/css/erp_theme.css"
+# web_include_js = "/assets/erp_theme/js/erp_theme.js"
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "erp_theme.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "erp_theme.install.before_install"
+# after_install = "erp_theme.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "erp_theme.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+app_include_css = [
+    "/assets/bdtheme/css/bdtheme.css",
+    "/assets/bdtheme/css/skin-blue.css",
+    "/assets/bdtheme/css/custom.css",
+    "/assets/bdtheme/css/temp.css",
+]
+app_include_js = [
+    "/assets/bdtheme/js/bdtheme.js",
+    "/assets/bdtheme/js/custom.js",
+    "/assets/js/bdtheme-template.min.js",
+]
+
+# include js, css files in header of web template
+web_include_css = "/assets/bdtheme/css/bdtheme-web.css"
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"erp_theme.tasks.all"
+# 	],
+# 	"daily": [
+# 		"erp_theme.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"erp_theme.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"erp_theme.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"erp_theme.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "erp_theme.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "erp_theme.event.get_events"
+# }
+
